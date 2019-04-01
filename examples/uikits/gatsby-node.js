a8c2f0eb389aef2518587d65c631f303250ed63e
slorber <lorber.sebastien@gmail.com>
2019-04-01 12:25:16 +0200

    WIP but unsuccessful attempt to support expo-web

--- examples/uikits/gatsby-node.js
+++ examples/uikits/gatsby-node.js
@@ -1,11 +1,86 @@
+const path = require('path')
+
+/*
+// Only compile files from the react ecosystem.
+const modules = [
+  path.join('node_modules', 'react-native'),
+  path.join('node_modules', 'react-navigation'),
+  path.join('node_modules', 'expo'),
+  path.join('node_modules', 'unimodules'),
+  path.join('node_modules', '@react'),
+  path.join('node_modules', '@expo'),
+  path.join('node_modules', '@unimodules'),
+
+  path.join('node_modules', 'native-base'),
+  path.join(
+    'node_modules',
+    'native-base',
+    'node_modules',
+    'react-native-vector-icons'
+  ),
+  path.join('node_modules', 'native-base-shoutem-theme'),
+]
+*/
+
+/*
+    test: /(([.]js?)|([.]web[.]js?)|([.]android[.]js?))$/,
+
+    include(inputPath) {
+      console.log('include', inputPath)
+      if (
+        inputPath.includes('react-native-ui-kitten') ||
+        inputPath.includes('fbjs')
+      ) {
+        console.log("false");
+        return false
+      }
+      for (const option of modules) {
+        if (inputPath.includes(option)) {
+          console.log('true')
+          return true
+        }
+      }
+      console.log('false')
+      return false
+    },
+ */
+
+/*
+test: /(([.]jsx?)|([.]web[.]jsx?)|([.]android[.]jsx?))$/,
+
+include(inputPath) {
+  for (const option of modules) {
+    if (inputPath.includes(option)) {
+      return inputPath
+    }
+  }
+  // Is inside the project and is not one of designated modules
+  if (
+    !inputPath.includes('node_modules') &&
+    inputPath.includes(babelRoot)
+  ) {
+    return inputPath
+  }
+  return null
+},
+*/
+
+/*
+    test: /(([.]jsx?)|([.]web[.]jsx?)|([.]android[.]jsx?))$/,
+    include: /node_modules/,
+    exclude: /node_modules[/\\](?!react-native-paper|react-native-vector-icons|react-native-safe-area-view|react-native-elements|react-native-ratings|react-native-drawer|react-native-keyboard-aware-scroll-view|react-native-tab-view|react-native-easy-grid|react-navigation|react-native-iphone-x-helper|native-base-shoutem-theme|native-base[/\\]node_modules[/\\]react-native-vector-icons|react-native-ui-kitten)/,
+ */
+
+
 // See https://callstack.github.io/react-native-paper/using-on-the-web.html
 exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
   const config = getConfig()
 
   config.module.rules.push({
-    test: /(([.]js)|([.]web[.]js)|([.]android[.]js))$/,
+    test: /(([.]jsx?)|([.]web[.]jsx?)|([.]android[.]jsx?))$/,
     include: /node_modules/,
     exclude: /node_modules[/\\](?!react-native-paper|react-native-vector-icons|react-native-safe-area-view|react-native-elements|react-native-ratings|react-native-drawer|react-native-keyboard-aware-scroll-view|react-native-tab-view|react-native-easy-grid|react-navigation|react-native-iphone-x-helper|native-base-shoutem-theme|native-base[/\\]node_modules[/\\]react-native-vector-icons|react-native-ui-kitten)/,
+
     use: {
       loader: 'babel-loader',
       options: {
@@ -29,6 +104,37 @@ exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
 
   // Required for ui kittens
   config.resolve.extensions.push('.android.js')
+
+  config.resolve.symlinks = false
+
+  config.resolve.alias = {
+    ...config.resolve.alias,
+
+
+    // Alias direct react-native imports to react-native-web
+    'react-native$': 'react-native-web',
+    // Add polyfills for modules that react-native-web doesn't support
+    // Depends on expo-asset
+    'react-native/Libraries/Image/AssetSourceResolver$':
+      'expo-asset/build/AssetSourceResolver',
+    'react-native/Libraries/Image/assetPathUtils$':
+      'expo-asset/build/Image/assetPathUtils',
+    'react-native/Libraries/Image/resolveAssetSource$':
+      'expo-asset/build/resolveAssetSource',
+    // Alias internal react-native modules to react-native-web
+    'react-native/Libraries/Components/View/ViewStylePropTypes$':
+      'react-native-web/dist/exports/View/ViewStylePropTypes',
+    'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter$':
+      'react-native-web/dist/vendor/react-native/NativeEventEmitter/RCTDeviceEventEmitter',
+    'react-native/Libraries/vendor/emitter/EventEmitter$':
+      'react-native-web/dist/vendor/react-native/emitter/EventEmitter',
+    'react-native/Libraries/vendor/emitter/EventSubscriptionVendor$':
+      'react-native-web/dist/vendor/react-native/emitter/EventSubscriptionVendor',
+    'react-native/Libraries/EventEmitter/NativeEventEmitter$':
+      'react-native-web/dist/vendor/react-native/NativeEventEmitter',
+  }
+
+  // console.debug('config', config)
 
   actions.replaceWebpackConfig(config)
 }
---
+++ examples/uikits/src/pages/expo.js
@@ -0,0 +1,39 @@
+
+/*
+import React, { useState } from 'react'
+
+import Layout from '../components/layout'
+import Example from '../components/example'
+import { Text } from 'react-native'
+import { LinearGradient } from 'expo'
+
+const LinearGradientExample = () => {
+  const [text, setText] = useState('')
+  return (
+    <Example title="LinearGradient">
+      <LinearGradient
+        colors={['#4c669f', '#3b5998', '#192f6a']}
+        style={{ padding: 15, alignItems: 'center', borderRadius: 5 }}
+      >
+        <Text
+          style={{
+            backgroundColor: 'transparent',
+            fontSize: 15,
+            color: '#fff',
+          }}
+        >
+          Sign in with Facebook
+        </Text>
+      </LinearGradient>
+    </Example>
+  )
+}
+
+const Elements = () => (
+  <Layout title={'expo-web examples'}>
+    <LinearGradientExample />
+  </Layout>
+)
+
+export default Elements
+*/
