19f4b231da6b54e53b267d301a013ad749d01649
Evan Bacon <baconbrix@gmail.com>
2019-10-23 14:27:04 -0700

    Updated Kittens examples

--- examples/uikits/gatsby-browser.js
+++ examples/uikits/gatsby-browser.js
@@ -1,6 +1,12 @@
+import { light as lightTheme, mapping } from '@eva-design/eva'
 import React from 'react'
 import { Provider as PaperProvider } from 'react-native-paper'
+import { ApplicationProvider } from 'react-native-ui-kitten'
 
 export const wrapRootElement = ({ element }) => {
-  return <PaperProvider>{element}</PaperProvider>
+  return (
+    <ApplicationProvider mapping={mapping} theme={lightTheme}>
+      <PaperProvider>{element}</PaperProvider>
+    </ApplicationProvider>
+  )
 }
