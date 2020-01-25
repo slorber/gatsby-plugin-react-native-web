663754ae2748ece5d6be11f19558bc2451a8f4a6
slorber <lorber.sebastien@gmail.com>
2020-01-25 17:49:10 +0100

    automatically add babel-plugin-remove-graphql-queries to expo js loader

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,6 +1,44 @@
 import { withUnimodules } from '@expo/webpack-config/addons'
 import { getModuleFileExtensions } from '@expo/webpack-config/env'
 
+// Temporary until better api is provided to get/customize expo js loader
+// See https://github.com/expo/examples/pull/39#discussion_r367702217
+const getExpoJsLoader = config => {
+  const {
+    conditionMatchesFile,
+    getRules,
+  } = require('@expo/webpack-config/utils')
+  const { resolve, join } = require('path')
+  const rules = getRules(config)
+  const jsLoaders = rules.filter(
+    ({ rule }) =>
+      rule.test &&
+      conditionMatchesFile(rule, resolve(join(__dirname, '../..', 'foo.js')))
+  )
+  // expo js loader is normally last added js loader (fragile but works)
+  const expoJsLoader = jsLoaders[jsLoaders.length - 1]
+  // console.log("expoJsLoader",JSON.stringify(expoJsLoader,null,2));
+  return expoJsLoader
+}
+
+const customizeExpoJsLoader = config => {
+  const expoJsLoader = getExpoJsLoader(config)
+
+  expoJsLoader.rule.use.options.plugins =
+    expoJsLoader.rule.use.options.plugins || []
+
+  // We need to add the gatsby static queries babel plugin to expo js loader
+  // otherwise gatsby will complain
+  // see https://github.com/slorber/gatsby-plugin-react-native-web/issues/23
+  expoJsLoader.rule.use.options.plugins.push([
+    'babel-plugin-remove-graphql-queries',
+    {},
+    'babel-plugin-remove-graphql-queries-for-expo-js-loader',
+  ])
+
+  return config;
+}
+
 const resolvableExtensions = () => getModuleFileExtensions('web')
 
 function onCreateBabelConfig({ actions }, options) {
@@ -18,7 +56,7 @@ function onCreateWebpackConfig({ actions, getConfig }) {
 
   let config
   try {
-    config = withUnimodules(gatsbyConfig)
+    config = customizeExpoJsLoader(withUnimodules(gatsbyConfig))
   } catch (error) {
     console.error(error)
     process.exit(1)
