ee50d030541c7378eea35231955d5938ae55379e
slorber <lorber.sebastien@gmail.com>
2020-01-28 14:50:10 +0100

    make it work with Docz

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -3,40 +3,55 @@ import { getModuleFileExtensions } from '@expo/webpack-config/env'
 
 // Temporary until better api is provided to get/customize expo js loader
 // See https://github.com/expo/examples/pull/39#discussion_r367702217
-const getExpoJsLoader = config => {
+const getExpoJsLoaderRule = config => {
   const {
     conditionMatchesFile,
-    getRules,
   } = require('@expo/webpack-config/utils')
   const { resolve, join } = require('path')
-  const rules = getRules(config)
-  const jsLoaders = rules.filter(
-    ({ rule }) =>
-      rule.test &&
-      conditionMatchesFile(rule, resolve(join(__dirname, '../..', 'foo.js')))
-  )
-  // expo js loader is normally last added js loader (fragile but works)
+
+  const rules = config.module.rules
+
+  // TODO bad way to find js loaders...
+  const jsLoaders = rules.filter(rule => {
+    if (rule.test) {
+      const relativeFoldersToTry = [
+        '../..',
+        '../../.docz', // Needed for Docz, as it has a nested structure
+      ]
+      return relativeFoldersToTry.some(relativeFolder =>
+        conditionMatchesFile(
+          rule,
+          resolve(join(__dirname, relativeFolder, 'foo.js'))
+        )
+      )
+    }
+    return false
+  })
+
+  // TODO bad way to find expo loaders among js loaders...
+  // expo js loader is normally last added js loader (fragile, but works)
   const expoJsLoader = jsLoaders[jsLoaders.length - 1]
+
   // console.log("expoJsLoader",JSON.stringify(expoJsLoader,null,2));
   return expoJsLoader
 }
 
 const customizeExpoJsLoader = config => {
-  const expoJsLoader = getExpoJsLoader(config)
+  const expoJsLoaderRule = getExpoJsLoaderRule(config)
 
-  expoJsLoader.rule.use.options.plugins =
-    expoJsLoader.rule.use.options.plugins || []
+  expoJsLoaderRule.use.options.plugins =
+    expoJsLoaderRule.use.options.plugins || []
 
   // We need to add the gatsby static queries babel plugin to expo js loader
   // otherwise gatsby will complain
   // see https://github.com/slorber/gatsby-plugin-react-native-web/issues/23
-  expoJsLoader.rule.use.options.plugins.push([
+  expoJsLoaderRule.use.options.plugins.push([
     'babel-plugin-remove-graphql-queries',
     {},
     'babel-plugin-remove-graphql-queries-for-expo-js-loader',
   ])
 
-  return config;
+  return config
 }
 
 const resolvableExtensions = () => getModuleFileExtensions('web')
