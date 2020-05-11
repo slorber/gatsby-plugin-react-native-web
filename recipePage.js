fe37cbbf07e1dc6fbfe1b0a9893259a15a2623bc
slorber <lorber.sebastien@gmail.com>
2020-05-11 12:24:34 +0200

    polish recipe

--- recipePage.js
+++ recipePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import { StyleSheet, TouchableOpacity, Text, View } from 'react-native'
+import { Video } from 'expo-av'
 
 const styles = StyleSheet.create({
   box: { padding: 10, margin: 10, borderWidth: 1, borderColor: 'black' },
@@ -26,6 +27,17 @@ const RNWPage = () => (
       <Text style={styles.buttonText}>Button</Text>
     </TouchableOpacity>
     <Link to="/">Go to home</Link>
+    <Video
+      source={{
+        uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+      }}
+      rate={1.0}
+      isMuted={true}
+      resizeMode="cover"
+      shouldPlay={true}
+      isLooping={true}
+      style={{ width: 500, marginTop: 30 }}
+    />
   </View>
 )
 
